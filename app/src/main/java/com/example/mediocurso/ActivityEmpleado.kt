2ca58996d1ba44416b87705056165abd07e7e4e2
Hans Davila <hansricardodavila@outlook.com>
2022-09-30 04:13:02 -0500

    Mostrar información despues de registro en empleado exitoso

--- app/src/main/java/com/example/mediocurso/ActivityEmpleado.kt
+++ app/src/main/java/com/example/mediocurso/ActivityEmpleado.kt
@@ -2,10 +2,50 @@ package com.example.mediocurso
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.mediocurso.databinding.ActivityEmpleadoBinding
+import com.google.firebase.firestore.FirebaseFirestore
+import com.google.firebase.firestore.QueryDocumentSnapshot
+import com.google.firebase.firestore.QuerySnapshot
 
 class ActivityEmpleado : AppCompatActivity() {
+    lateinit var binding: ActivityEmpleadoBinding
+    val db : FirebaseFirestore = FirebaseFirestore.getInstance()
+
+    companion object{
+        val NOMBRE_TABLA = "users"
+        val CAMPO_CORREO = "correo"
+        val CAMPO_NOMBRE = "nombre"
+        val CAMPO_EMPRESA = "empresa"
+        val CAMPO_TELEFONO = "telefono"
+        val CAMPO_PUESTO = "puesto empresa"
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_empleado)
+
+        binding = ActivityEmpleadoBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        //setup
+
+        val bundle: Bundle? = intent.extras
+        val email: String? = bundle?.getString(CAMPO_CORREO)
+
+        db.collection(NOMBRE_TABLA).whereEqualTo(CAMPO_CORREO,email).get().addOnSuccessListener { documents ->
+            val users = mutableListOf<QueryDocumentSnapshot>()
+            for(document in documents){
+                users.add(document)
+            }
+
+            val user = users.first()
+
+            binding.tvName.text =  user.get(CAMPO_NOMBRE).toString()
+            binding.correoTv.text =  user.get(CAMPO_CORREO).toString()
+            binding.telefono.text =  user.get(CAMPO_TELEFONO).toString()
+            binding.empresa.text =  user.get(CAMPO_EMPRESA).toString()
+            binding.puesto.text =  user.get(CAMPO_PUESTO).toString()
+            binding.idEmpleado.text = user.id
+        }
+
     }
 }
