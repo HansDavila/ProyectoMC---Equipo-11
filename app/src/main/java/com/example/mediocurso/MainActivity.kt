fd738fbc82fcbf0275caddd4adc6fbbe87d73600
Hans Davila <hansricardodavila@outlook.com>
2022-09-30 01:59:42 -0500

    Crear usuario done

--- app/src/main/java/com/example/mediocurso/MainActivity.kt
+++ app/src/main/java/com/example/mediocurso/MainActivity.kt
@@ -3,7 +3,9 @@ package com.example.mediocurso
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
 import com.example.mediocurso.databinding.ActivityMainBinding
+import com.google.firebase.auth.FirebaseAuth
 
 class MainActivity : AppCompatActivity() {
 
@@ -18,6 +20,32 @@ class MainActivity : AppCompatActivity() {
             val intentRegisterActivity = Intent(this, RegisterActivity::class.java)
             startActivity(intentRegisterActivity)
 
+
+
+            //setup
+            setup()
+        }
+
+
+    }
+
+    private fun setup() {
+
+
+
+        binding.btnLogin.setOnClickListener{
+            if (binding.tvEmail.text.isNotEmpty() && binding.tvPassword.text.isNotEmpty()){
+
+                FirebaseAuth.getInstance().signInWithEmailAndPassword(binding.tvEmail.text.toString(),
+                    binding.tvPassword.text.toString()).addOnCompleteListener{
+
+                    if(it.isSuccessful){
+
+                    }else{
+                        var toast = Toast.makeText(this, "ERROR AL INICIAR SESION CON USUARIO", Toast.LENGTH_SHORT).show()
+                    }
+                }
+            }
         }
     }
 }
--- app/src/main/java/com/example/mediocurso/RegisterActivity.kt
+++ app/src/main/java/com/example/mediocurso/RegisterActivity.kt
@@ -2,14 +2,69 @@ package com.example.mediocurso
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
 import com.example.mediocurso.databinding.ActivityRegisterBinding
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.firestore.FirebaseFirestore
 
 class RegisterActivity : AppCompatActivity() {
     lateinit var  binding: ActivityRegisterBinding
+    val db : FirebaseFirestore = FirebaseFirestore.getInstance()
+    var puesto = ""
+
+    companion object{
+        val NOMBRE_TABLA = "users"
+        val CAMPO_CORREO = "correo"
+        val CAMPO_NOMBRE = "nombre"
+        val CAMPO_EMPRESA = "empresa"
+        val CAMPO_TELEFONO = "telefono"
+        val CAMPO_PUESTO = "puesto empresa"
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityRegisterBinding.inflate(layoutInflater)
         setContentView(binding.root)
+
+
+        binding.btnRegistrar.setOnClickListener{
+            if (binding.tvNombre.text.isNotEmpty() && binding.tvIdEmpleado.text.isNotEmpty() && binding.tvPassword.text.isNotEmpty() && binding.tvEmpresa.text.isNotEmpty() && binding.tvEmpresa.text.isNotEmpty()
+                && binding.tvCorreo.text.isNotEmpty()){
+
+                var switchState = binding.switchPuesto.isActivated
+
+
+                FirebaseAuth.getInstance().createUserWithEmailAndPassword(binding.tvCorreo.text.toString(),     //crear auntenticador con datos de usuario
+                    binding.tvPassword.text.toString()).addOnCompleteListener{
+
+
+                    if(it.isSuccessful){
+
+                        if(switchState){
+                            puesto = "admin"
+                        }else{
+                            puesto = "empleado"
+                        }
+
+                        //CREACION DE BASE DE DATOS
+                        db.collection(NOMBRE_TABLA).document(binding.tvIdEmpleado.text.toString()).set(
+                            hashMapOf(
+
+                                CAMPO_EMPRESA to binding.tvEmpresa.text.toString(),
+                                CAMPO_NOMBRE to binding.tvNombre.text.toString(),
+                                CAMPO_TELEFONO to binding.tvTelefono.text.toString(),
+                                CAMPO_CORREO to binding.tvCorreo.text.toString(),
+                                CAMPO_PUESTO to puesto
+                            )
+                        )
+
+                        var toast = Toast.makeText(this, "USUARIO ${binding.tvNombre.text.toString()} GENERADO EXITOSAMENTE", Toast.LENGTH_SHORT).show()
+
+                    }else{
+                        var toast = Toast.makeText(this, "INGRESE TODOS LOS DATOS", Toast.LENGTH_SHORT).show()
+                    }
+                }
+            }
+        }
     }
 }
