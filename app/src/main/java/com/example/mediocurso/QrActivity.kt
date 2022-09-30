26aeacb243b834152a923624dbba7a5995064078
Hans Davila <hansricardodavila@outlook.com>
2022-09-30 18:44:42 -0500

    Activity Codigo QR agregado

---
+++ app/src/main/java/com/example/mediocurso/QrActivity.kt
@@ -0,0 +1,12 @@
+package com.example.mediocurso
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class QrActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_qr)
+    }
+}
